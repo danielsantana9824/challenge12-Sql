519962c1fa93d38ca5716b67ff4bede3fa57e155
danielsantana9824 <danielsantana9824@gmail.com>
2024-10-27 22:08:24 -0500

    seeds
    only data to examples

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,7 +13,4 @@ INSERT INTO employee(first_name, last_name, role_id )
 VALUES('John', 'Doe',1),
       ('Mike', 'Chan',2),
       ('Ashley', 'Rodriguez',3);
-
-
- UPDATE  employee SET manager_id =1  WHERE id=2;
-    
+ 
